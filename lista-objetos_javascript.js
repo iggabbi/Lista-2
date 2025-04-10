29bdd25511be49258c3ec538e23ef063ebea6f36
suguruswifey <gabriela.tizeu@aluno.ifsp.edu.br>
2025-04-10 20:40:12 -0300

    Create lista-objetos_javascript.js

---
+++ lista-objetos_javascript.js
@@ -0,0 +1,31 @@
+// Exercício 1
+
+const carro = {
+  marca: “Toyota”,
+  modelo: “Corolla”,
+    ano: 2025,
+};
+console.log(carro.marca);
+console.log(carro.modelo);
+console.log(carro.ano);
+
+
+// Exercício 2
+
+const usuario = {
+  nome: "Fernanda",
+  email: "fernanda@email.com",
+  idade: 25
+};
+delete usuario.email
+console.log(usuario);
+
+
+// Exercício 3
+
+const pessoa = {
+  nome: "Carlos",
+  idade: 29
+};
+pessoa.idade = 31
+console.log(pessoa.idade);
